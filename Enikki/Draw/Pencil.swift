c94a98aa968bdcd791ce74e0746c78fc4ff1ed3a
tiking76 <716yosiki1999@gmail.com>
2021-08-10 14:48:14 +0900

    create sketch View

--- Enikki/Draw/Pencil.swift
+++ Enikki/Draw/Pencil.swift
@@ -5,4 +5,22 @@
 //  Created by tiking on 2021/08/10.
 //
 
-import Foundation
+import SwiftUI
+import PencilKit
+
+struct NewPaint {
+    @Binding var canvasView: PKCanvasView
+}
+
+extension NewPaint: UIViewRepresentable {
+    
+    typealias UIViewType = PKCanvasView
+
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvasView.drawingPolicy = .anyInput
+        canvasView.tool = PKInkingTool(.pen, color: .black, width: 4.0)
+        return canvasView
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {}
+}
--- Enikki/Draw/SketchView.swift
+++ Enikki/Draw/SketchView.swift
@@ -6,15 +6,11 @@
 //
 
 import SwiftUI
+import PencilKit
 
 struct SketchView: View {
+    @State private var canvasView = PKCanvasView()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct SketchView_Previews: PreviewProvider {
-    static var previews: some View {
-        SketchView()
+        NewPaint(canvasView: $canvasView)
     }
 }
