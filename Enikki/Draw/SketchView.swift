16c052ffc49917f4bf16bdb66b9355cb33447b0f
tiking76 <716yosiki1999@gmail.com>
2021-08-10 14:28:04 +0900

    add sketchView

---
+++ Enikki/Draw/SketchView.swift
@@ -0,0 +1,20 @@
+//
+//  SketchView.swift
+//  Enikki
+//
+//  Created by tiking on 2021/08/10.
+//
+
+import SwiftUI
+
+struct SketchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SketchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SketchView()
+    }
+}
