cf6ab9a6eeb39d853fc229e34259275aaefe7a9d
tiking76 <716yosiki1999@gmail.com>
2021-08-10 14:26:03 +0900

    fix

--- Enikki/Extensions/Color.swift
+++ Enikki/Extensions/Color.swift
@@ -8,26 +8,6 @@ extension UIColor {
     static let primary = UIColor(red: 229 / 255, green: 248 / 255, blue: 245 / 255, alpha: 1)
 }
 
-// test color
-enum ColorAssets {
-    case lightOrange
-    case darkRed
-    var name: String {
-        switch self {
-        case .lightOrange:
-            return "lightOrange"
-        case .darkRed:
-            return "darkRed"
-        }
-    }
-    var color: Color {
-        return Color(name)
-    }
-    var uiColor: UIColor? {
-        return UIColor(named: name)
-    }
-}
-
 extension Color {
     static let darkRed = Color(UIColor.darkRed)
     static let primary = Color(UIColor.primary)
